2a324b37baa840154b2fca6c60e5df15d2b55da1
cbracketdash <cbracketdash@gmail.com>
2020-12-28 15:13:47 -0800

    update?

---
+++ build.bat
@@ -0,0 +1,5 @@
+python --version || echo Make sure Python is installed !
+python -m pip --version || echo Make sure Pip is installed !
+python -m pip install numpy
+python -m pip install matplotlib
+
